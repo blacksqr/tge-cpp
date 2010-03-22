c63c2c5fa70152d904f8632cf1d14d67453cf04d
jeffreybian <jeffreybian@99f00b9d-c5df-25c8-5ecf-577a790fa8fa>
2010-03-22 12:36:27 +0000

    2010-3-22

--- trunk/tgeEngine.cpp
+++ trunk/tgeEngine.cpp
@@ -5,13 +5,23 @@
 tge::Engine* Ogre::Singleton<class tge::Engine>::ms_Singleton = NULL;
 namespace tge
 {
-	Engine::Engine(void)
+	Engine::Engine(void) :
+	mDefContext(NULL)
 	{
 		createInterpreter("Default");
 		setDefaultObjContext("Default");
 	}
 	Engine::~Engine(void)
 	{
+		/** Clear upon deletion. */
+		if (mInterpMap.empty())
+		{
+			delete getDefaultObjContext();
+		}
+		else
+		{
+			// TODO do more cleaning here
+		}
 	}
 	void Engine::init(void)
 	{
@@ -21,18 +31,47 @@ namespace tge
 	{
 	}
 
-	bool Engine::createInterpreter(const String& name)
+	Interpreter* Engine::createInterpreter(const String& name) throw (tge::except)
 	{
-		mInterpMap.insert(std::make_pair(name, new Interpreter));
-		return true;
+		Interpreter* interp = new Interpreter;
+		if (interp==NULL)
+		{
+			throw (except("Cannot create interpreter."));
+		}
+		std::pair<InterpreterMap::iterator, bool> ret = 
+		mInterpMap.insert(InterpreterMap::value_type(name, interp));
+		if ( false==ret.second )
+		{
+			String s("Interpreter with name ");
+			s.append(name).append(" already exists.");
+			throw (except(s));
+		}
+		return interp;
 	}
 
-	Interpreter* Engine::getInterpreter(const String& name)
+	Interpreter* Engine::getInterpreter(const String& name) const
 	{
-		return mInterpMap[name];
+		Interpreter* ret = NULL;
+		if (name.empty())
+		{
+			ret = getDefaultObjContext();
+		}
+		else
+		{
+			InterpreterMap::const_iterator it = mInterpMap.find(name);
+			if (it!=mInterpMap.end())
+			{
+				ret = it->second;
+			}
+			else
+			{
+				ret = NULL;
+			}
+		}
+		return ret;
 	}
 
-	bool Engine::registerScriptIntfFactory(const String& interpreterName, ScriptIntfFactory* factory)
+	bool Engine::registerScriptIntfFactory(ScriptIntfFactory* factory, const String& interpreterName)
 	{
 		bool ret = true;
 		Interpreter* interp = getInterpreter(interpreterName);
@@ -41,6 +80,24 @@ namespace tge
 		{
 			return false;
 		}
+		// Checks if this factory is already registerd.
+		for (SIPtrInterpPtrMap::const_iterator it = mSItoInterpMap.begin();
+			it!=mSItoInterpMap.end();
+			++it)
+		{
+			ScriptIntfFactory* _f = static_cast<ScriptIntfFactory*>(it->first);
+			if ( _f==factory || typeid(_f) == typeid(factory) ) 
+			{
+				if ( interp==static_cast<Interpreter*>(it->second) )
+				{
+
+				}
+			}
+			
+		}
+
+		mSItoInterpMap.insert(std::make_pair(factory, interp));
+
 
 		factory->setInterp(interp);
 
@@ -48,26 +105,17 @@ namespace tge
 
 		for (StringVector::iterator i = svec.begin(); i!= svec.end(); ++i)
 		{
-			// String createCmd = (*i) + String("_create");
-			// String destroyCmd = (*i) + String("_destroy");
-			
 			String cmd = (*i);
 			interp->registerCmd(cmd, factory);
-
-			// interp->registerCmd(createCmd, factory);
-			// interp->registerCmd(destroyCmd, factory);
 		}
-
-		// Tcl_GetGlobalNamespace
 		return ret;
-
 	}
 
-	Result Engine::eval(const String& str)
+	Result Engine::eval(const String& str, const String& interpreterName)
 	{
 		return mDefContext->eval(str);
 	}
-	Result Engine::eval(Obj* obj) 
+	Result Engine::eval(Obj* obj, const String& interpreterName) 
 	{
 		return mDefContext->eval(obj);
 	}
@@ -84,11 +132,11 @@ namespace tge
 	{
 		mDefContext->removeVar(name);
 	}
-	bool Engine::registerGlobalCmd(const String& cmdName, Cmd* cmd)
+	bool Engine::registerGlobalCmd(const String& cmdName, Cmd* cmd, const String& interpName)
 	{
 		return mDefContext->registerCmd(cmdName, cmd);
 	}
-	bool Engine::unregisterGlobalCmd(const String& cmdName)
+	bool Engine::unregisterGlobalCmd(const String& cmdName, const String& interpName)
 	{
 		return mDefContext->unregisterCmd(cmdName);
 	}
@@ -101,7 +149,7 @@ namespace tge
 		return mDefContext;
 	}
 
-	bool Engine::unregisterScriptIntfFactory(ScriptIntfFactory* factory)
+	bool Engine::unregisterScriptIntfFactory(ScriptIntfFactory* factory, const String& interpName)
 	{
 		bool ret = true;
 		StringVector svec = factory->getCapabilities();
@@ -114,7 +162,7 @@ namespace tge
 		return ret;
 	}
 
-	bool Engine::commandExists(const String& cmdName)
+	bool Engine::commandExists(const String& cmdName, const String& interpName)
 	{
 		return mDefContext->commandExists(cmdName);
 	}
