7ebcd4bb6679bc0af12f40e2a788c00c78f03516
jeffreybian <jeffreybian@99f00b9d-c5df-25c8-5ecf-577a790fa8fa>
2010-03-22 12:36:40 +0000

    2010-3-22

--- trunk/tgeCmd.h
+++ trunk/tgeCmd.h
@@ -14,7 +14,7 @@ namespace tge
 		virtual ~Cmd(void);
 
 		/** Override this method. 
-			@note isNull() check shoud perform for arg1, arg2 and argRest to determine whether
+			@note isNull() check should perform for arg1, arg2 and argRest to determine whether
 			they really exist. The script engine will set the non-existing param null.
 		*/
 		virtual CmdStatus run(const String& cmdName, const Obj& arg1, const Obj& arg2, const Obj& argRest, size_t argRestc);
